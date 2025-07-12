55b6873426aa4c65ce8fc49b7e0c4598af38ee09
hectormare <laro.1962@hotmail.com>
2025-07-12 11:56:52 +0100

    Actualizo Sobre.js con contenido literario del autor

--- components/Sobre.js
+++ components/Sobre.js
@@ -1,1 +1,43 @@
-export default function Sobre() { return <section>Sobre</section>; }
+export default function Sobre(
+
+return (
+
+<section id="sobre" classN <div className="max-w-2x
+
+<h2 className="text-4x
+
+<div className="mb-6">
+
+{/* Aquí puedes inse <img
+
+src="/images/hecto alt="Foto de Hécto className="w-40 h-
+
+‹/div>
+
+<p className="text-lg
+
+Bajo el seudónimo de
+
+‹/p>
+
+<p className="text-lg
+
+Con una trayectoria
+
+‹/p>
+
+<p className="text-lg
+
+Su estilo se disting
+
+‹p>
+
+‹/div>
+
+< section>
+
+) ;
+
+
+
+}
